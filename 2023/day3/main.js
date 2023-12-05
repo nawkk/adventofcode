7d6c951cbb845c6e9033fe69b24a98dce0b48f79
Nawkk <im.nawkk@gmail.com>
2023-12-04 23:20:16 -1000

    updating day3 main

--- 2023/day3/main.js
+++ 2023/day3/main.js
@@ -2,33 +2,54 @@ const fs = require("fs");
 
 const parse = (source) => source.split(/\r?\n/).filter(Boolean);
 
-const data = parse(fs.readFileSync("example.txt", "utf-8"));
-// const data = parse(fs.readFileSync("input.txt", "utf-8"));
+// const data = parse(fs.readFileSync("example.txt", "utf-8"));
+const data = parse(fs.readFileSync("input.txt", "utf-8"));
 
-console.log(data);
+let nums = [];
+let map = {};
 
-// example has 1-10
-// input has 1-140
-
-
-data.forEach((d, dIdx) => {
-	for (let i = 0; i < d.length; i++) {
-		if (isNaN(d.charAt(i)) && d.charAt(i) !== '.') {
-			// symbol found
-			// check hitbox
-			let prevD = dIdx - 1;
-			let nextD = dIdx + 1;
-			console.log(data[prevD].charAt(i-1));
-			console.log(data[prevD].charAt(i));
-			console.log(data[prevD].charAt(i+1));
-
-			console.log(data[dIdx].charAt(i-1));
-			console.log(data[dIdx].charAt(i));
-			console.log(data[dIdx].charAt(i+1));
-
-			console.log(data[nextD].charAt(i-1));
-			console.log(data[nextD].charAt(i));
-			console.log(data[nextD].charAt(i+1));
+data.forEach((dStr, i) => {
+	let start = -1;
+	let keys = [];
+	for (let j = 0; j < dStr.length; j++) {
+		if (!isNaN(dStr.charAt(j))) {
+			if (start === -1) {
+				start = j;
+			}
+			keys.push(i + '' +  j);
+		} else {
+			if (start !== -1) {
+				nums.push(parseInt(dStr.substring(start, j)));
+				const val = nums.length - 1;
+				keys.forEach(k => {
+					map[k] = val;
+				});
+				start = -1;
+			}
+			keys = [];
 		}
 	}
 });
+
+let sum = 0;
+data.forEach((dStr, i) => {
+	for (let j = 0; j < dStr.length; j++) {
+		if (isNaN(dStr.charAt(j)) && dStr.charAt(j) !== '.') {
+			let left = [i-1, i, i+1];
+			let right = [j-1, j, j+1];
+			let idx = -1;
+			left.forEach(l => {
+				right.forEach(r => {
+					if (!isNaN(data[l].charAt(r))) {
+						idx = map[l + '' + r];
+						sum += nums[idx];
+						console.log(sum);
+						nums[idx] = 0;
+					}
+				});
+			});
+		}
+	}
+});
+
+console.log(sum);
