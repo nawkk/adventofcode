49ac71d8e1c8c437aa7133675521aff625d48fcc
Nawkk <im.nawkk@gmail.com>
2023-12-05 22:47:32 -1000

    solving d4p2

--- 2023/day4/main.js
+++ 2023/day4/main.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 const parse = (source) => source.split(/\r?\n/).filter(Boolean);
 
-const data = parse(fs.readFileSync("example.txt", "utf-8"));
-// const data = parse(fs.readFileSync("input.txt", "utf-8"));
+// const data = parse(fs.readFileSync("example.txt", "utf-8"));
+const data = parse(fs.readFileSync("input.txt", "utf-8"));
 
 // did not account for duplicates 
 function partOne(data) {
@@ -35,4 +35,46 @@ function partOne(data) {
 	return sum;
 }
 
-console.log(partOne(data));
+function partTwo(data) {
+	let sum = 0;
+	let countArr = [];
+	data.forEach((d, i) => {
+		const nums = d.split(':')[1];
+		const [ winning, mine ] = nums.split('|');
+		const winningArr = winning.split(' ');
+		const mineArr = mine.split(' ');
+		let map = {};
+		let matches = 0;
+
+		winningArr.forEach(wn => {
+			if (wn !== '') {
+				map[wn] = 0;
+			}
+		});
+		mineArr.forEach(v => {
+			if (v !== '') {
+				let val = map[v];
+				if (val !== undefined && val === 0) {
+					map[v]++;
+					matches++;
+				}
+			}
+		});
+		if (countArr[i] === undefined) {
+			countArr[i] = 0;
+		}
+		countArr[i]++;
+		while (matches > 0) {
+			if (countArr[i+matches] === undefined) {
+				countArr[i+matches] = 0;
+			}
+			countArr[i+matches] += countArr[i];
+			matches--;
+		}
+		sum += countArr[i];
+	});
+	return sum;
+}
+
+// console.log(partOne(data));
+console.log(partTwo(data));
