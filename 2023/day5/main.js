238af7b260d37092e1a58b9f8f30bf6930ef3718
Nawkk <im.nawkk@gmail.com>
2023-12-09 17:11:53 -1000

    solving part 1 for day 5... but did not consider performance at all

--- 2023/day5/main.js
+++ 2023/day5/main.js
@@ -2,12 +2,40 @@ const fs = require("fs");
 
 const parse = (source) => source.split(/\r?\n/).filter(Boolean);
 
-const data = parse(fs.readFileSync("example.txt", "utf-8"));
-// const data = parse(fs.readFileSync("input.txt", "utf-8"));
+// const data = parse(fs.readFileSync("example.txt", "utf-8"));
+const data = parse(fs.readFileSync("input.txt", "utf-8"));
 
-console.log(data);
+// [dest, src, range]
 
+// Performance could be improved... greatly
+function partOne(data) {
+	const seeds = data.shift().split(': ')[1].split(' ');
 
+	let low = -1;
+	seeds.forEach(seed => {
+		let temp = seed;
+		let section = 0;
+		let goNext = false;
+		data.forEach((x) => {
+			if (isNaN(x.charAt(0))) {
+				section++;
+				goNext = false;
+			} else if (goNext) {
+				return;
+			} else {
+				const [dest, src, range] = x.split(' ').map(Number);
+				if (temp >= src && temp <= src + range - 1) {
+					temp = temp - src + dest;
+					goNext = true;
+				}
+			}
+		});
+		if (section === 7) {
+			low = (low === -1 || temp < low) ? temp : low;
+		}
 
-// Need to return LOWEST location number that 
-	// corresponds to any of the initial seed numbers 
+	})
+
+	return low;
+}
+console.log(partOne(data));
